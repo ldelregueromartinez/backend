69ad42ca398d8c74e775560e6eac80436bcb3ee9
Lucas del Reguero Martinez <ldelregueromartinez@siu.edu.ar>
2023-11-15 22:39:41 -0300

    2

--- src/database/database.js
+++ src/database/database.js
@@ -4,8 +4,8 @@ import Sequelize from "sequelize";
 
 // conexion con la base  https://sequelize.org/docs/v6/getting-started/#connecting-to-a-database
 
-export const sequelize = new Sequelize("salute", "postgres", "root", {
-    host: "localhost",
+export const sequelize = new Sequelize("salute", "fl0user", "DbYg9l4HsFry", {
+    host: "ep-jolly-bread-41790663.us-east-2.aws.neon.fl0.io",
     dialect: "postgres",
 });
 
