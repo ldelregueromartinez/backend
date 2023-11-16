feca5b2e89099aa683b368f26caf58df7bc8a10a
Lucas <127217353+ldelregueromartinez@users.noreply.github.com>
2023-11-16 09:12:05 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,8 +16,9 @@ import asientosRoutes from './routes/asientos.routes.js';
 app.use((req,res, next)=>{
     res.setHeader('Access-Control-Allow-Origin', 'https://backend-dev-ateq.4.us-1.fl0.io');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization');
     next();
+    
 });
 
 
